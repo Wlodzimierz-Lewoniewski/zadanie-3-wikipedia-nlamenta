9b1e7589e25161be8442861e4cbf66cdbfd7a490
nlamenta <161578683+nlamenta@users.noreply.github.com>
2024-10-29 18:11:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,31 +17,35 @@ def wyciagnij(kategoria):
 
     # wyszukiwanie pierwszych dwóch artykułów w kategorii
     artykuly = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', html)
-    artykuly = artykuly[:2]  # ograniczenie
+    artykuly = artykuly[:2]  # ograniczenie do pierwszych dwóch artykułów
 
     for artykul in artykuly:
-        tytul = artykul[1]  # Poprawna zmiana, aby użyć tytulu z aktualnego artykułu
+        tytul = artykul[1]  # Tytuł artykułu
         artykul_url = f"https://pl.wikipedia.org{artykul[0]}"
 
         odp_artykul = urlopen(artykul_url)
         artykul_zawartosc = odp_artykul.read().decode('utf-8')
 
+        # Znajdowanie linków wewnętrznych
         linki = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', artykul_zawartosc)
         linki = [(link[1], f"https://pl.wikipedia.org{link[0]}") for link in linki[:5]]
 
+        # Znajdowanie obrazków
         zdj_linki = re.findall(r'<img.*?src="(//upload.wikimedia.org.*?)".*?>', artykul_zawartosc)
         zdj_linki = zdj_linki[:3]  # Pierwsze 3 obrazy
         zdj_linki = [f"https:{link}" for link in zdj_linki]  # Dodajemy protokół
 
+        # Znajdowanie źródeł zewnętrznych
         linki2 = re.findall(r'<a href="(https?://[^"]+)"', artykul_zawartosc)
         linki2 = linki2[:3]  # Pierwsze 3 źródła
 
+        # Znajdowanie przypisanych kategorii
         kategorie = re.findall(r'<div class="catlinks">.*?<ul>(.*?)</ul>', artykul_zawartosc, re.DOTALL)
         kategorie = re.findall(r'<li><a href="[^"]*?">([^<]*?)</a>', kategorie[0]) if kategorie else []
         kategorie = kategorie[:3]  # Pierwsze 3 kategorie
 
-        # wyświetlanie
-        print(f"{tytul} | {artykul_url} | ", end="")
+        # Wyświetlanie w odpowiednim formacie
+        print(f"{tytul} | ", end="")
         print(" | ".join([link[0] for link in linki]), end=" | ")
         print(" | ".join(zdj_linki), end=" | ")
         print(" | ".join(linki2), end=" | ")
