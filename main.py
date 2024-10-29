df090775a3c9a12e809d2efe99796fab69cd2fd4
nlamenta <161578683+nlamenta@users.noreply.github.com>
2024-10-29 17:17:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,51 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import re
+from urllib.request import urlopen
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+# funkcja do pobrania i przetworzenia artykułów z danej kategorii
+def wyciagnij(kategoria):
+    url = "https://pl.wikipedia.org/wiki/Kategoria:" + kategoria.replace(' ', '_')
+    odp = urlopen(url)
+    html = odp.read().decode("utf-8")
+
+    # wyszukiwanie pierwszych dwóch artykułów w kategorii
+    artykuly = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', html)
+    artykuly = artykuly[:2]  # ograniczenie
+
+    for artykul in artykuly:
+        tytul = artykuly[1]
+        artykul_url = f"https://pl.wikipedia.org{artykul[0]}"
+
+        odp_artykul = urlopen(artykul_url)
+        artykul_zawartosc = odp_artykul.read().decode('utf-8')
+
+        linki = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', artykul_zawartosc)
+        linki = [(link[1], f"https://pl.wikipedia.org{link[0]}") for link in linki[:5]]
+
+        zdj_linki = re.findall(r'<img.*?src="(//upload.wikimedia.org.*?)".*?>', artykul_zawartosc)
+        zdj_linki = zdj_linki[:3]  # Pierwsze 3 obrazy
+        zdj_linki = [f"https:{link}" for link in zdj_linki]  # Dodajemy protokół
+
+        linki2 = re.findall(r'<a href="(https?://[^"]+)"', artykul_zawartosc)
+        linki2 = linki2[:3]  # Pierwsze 3 źródła
+
+        kategorie = re.findall(r'<div class="catlinks">.*?<ul>(.*?)</ul>', artykul_zawartosc, re.DOTALL)
+        kategorie = re.findall(r'<li><a href="[^"]*?">([^<]*?)</a>', kategorie[0]) if kategorie else []
+        kategorie = kategorie[:3]  # Pierwsze 3 kategorie
+
+        # wyświetlanie
+        print(f"{tytul} | {artykul_url} | ", end="")
+
+        print(" | ".join([link[0] for link in linki]), end=" | ")
+
+        print(" | ".join(zdj_linki), end=" | ")
+
+        print(" | ".join(linki2), end=" | ")
+
+        print(" | ".join(kategorie))
+
+        print("\n" + "-" * 50 + "\n")
+
+# input
+kategoria_input = input("Podaj nazwę kategorii na Wikipedii: ").strip()
+wyciagnij(kategoria_input)
