e2e617ee6e7dc45731eb86af165753a190d25de1
nlamenta <161578683+nlamenta@users.noreply.github.com>
2024-11-02 19:33:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
 import re
-from urllib.request import urlopen
 from urllib.parse import quote
-import sys
-import io
-
-# Ustawienie odpowiedniego kodowania dla konsoli
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+import requests
 
 # funkcja do pobrania i przetworzenia artykułów z danej kategorii
 def wyciagnij(kategoria):
-    # Przygotowanie URL do kategorii z kodowaniem
+    kategoria = kategoria.replace(" ", "_")
     kategoria_encoded = quote(kategoria)
-    url = "https://pl.wikipedia.org/wiki/Kategoria:" + kategoria_encoded
-    odp = urlopen(url)
-    html = odp.read().decode("utf-8")
+    url = f"https://pl.wikipedia.org/wiki/Kategoria:{kategoria_encoded}"
+    response = requests.get(url).text
 
     # wyszukiwanie pierwszych dwóch artykułów w kategorii
-    artykuly = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', html)
-    artykuly = artykuly[:2]  # ograniczenie do pierwszych dwóch artykułów
+    main_content = response[re.search(re.compile(r'mw-category-generated'), response).start():]
+    artykuly = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', main_content)[:2]
+
+    if not artykuly:
+        print("Nie znaleziono artykułów w tej kategorii.")
+        return
 
     for artykul in artykuly:
-        tytul = artykul[1]  # Tytuł artykułu
+        tytul = artykul[1]
         artykul_url = f"https://pl.wikipedia.org{artykul[0]}"
+        response_artykul = requests.get(artykul_url).text
 
-        odp_artykul = urlopen(artykul_url)
-        artykul_zawartosc = odp_artykul.read().decode('utf-8')
+        # znajdowanie linków wewnętrznych
+        start = re.search(re.compile(r'id="mw-content-text"'), response_artykul).start()
+        text_content = response_artykul[start:]
+        linki = re.findall(re.compile(r'href="[^:]+?".+?title="(.*?)"'), text_content)[:5]
 
-        # Znajdowanie linków wewnętrznych
-        linki = re.findall(r'<a href="(/wiki/[^":#]*?)".*?>(.*?)</a>', artykul_zawartosc)
-        linki = [(link[1], f"https://pl.wikipedia.org{link[0]}") for link in linki[:5]]
+        # znajdowanie obrazków
+        image_text = re.search(re.compile(r'mw-content-text(.+?)<noscript>', re.DOTALL), text_content).group()
+        images = re.findall(re.compile(r'<img.+?src="(.+?)"'), image_text)
+        images = images[:3]  # Pierwsze 3 obrazy
+        images = [f"https:{img}" for img in images]
 
-        # Znajdowanie obrazków
-        zdj_linki = re.findall(r'<img.*?src="(//upload.wikimedia.org.*?)".*?>', artykul_zawartosc)
-        zdj_linki = zdj_linki[:3]  # Pierwsze 3 obrazy
-        zdj_linki = [f"https:{link}" for link in zdj_linki]  # Dodajemy protokół
+        # znajdowanie źródeł zewnętrznych
+        sources_text = re.search(re.compile(r'id="Przypisy"(.+)', re.DOTALL), text_content)
+        if sources_text:
+            sources_text = re.search(re.compile(r'class="references"(.+?)</ol>', re.DOTALL), sources_text.group()).group()
+            linki2 = re.findall(re.compile(r'"(http.+?)"'), sources_text)[:3]
+        else:
+            linki2 = []
 
-        # Znajdowanie źródeł zewnętrznych
-        linki2 = re.findall(r'<a href="(https?://[^"]+)"', artykul_zawartosc)
-        linki2 = linki2[:3]  # Pierwsze 3 źródła
+        # znajdowanie przypisanych kategorii
+        categories_text = re.search(re.compile('mw-normal-catlinks(.+?)</div>'), text_content).group()
+        kategorie = re.findall(re.compile(r'<li.+?>(.+?)</a></li>'), categories_text)[:3]
 
-        # Znajdowanie przypisanych kategorii
-        kategorie = re.findall(r'<div class="catlinks">.*?<ul>(.*?)</ul>', artykul_zawartosc, re.DOTALL)
-        kategorie = re.findall(r'<li><a href="[^"]*?">([^<]*?)</a>', kategorie[0]) if kategorie else []
-        kategorie = kategorie[:3]  # Pierwsze 3 kategorie
-
-        # Wyświetlanie w odpowiednim formacie
-        print(f"{tytul} | ", end="")
-        print(" | ".join([link[0] for link in linki]), end=" | ")
-        print(" | ".join(zdj_linki), end=" | ")
+        # wyświetlanie
+        print(" | ".join(linki), end=" | ")
+        print(" | ".join(images), end=" | ")
         print(" | ".join(linki2), end=" | ")
         print(" | ".join(kategorie))
         print("\n" + "-" * 50 + "\n")
